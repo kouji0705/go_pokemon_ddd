1c71eba4a5824993465fc60c062b08fd97f806ba
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2025-02-11 17:21:15 +0900

    add:

--- cmd/main.go
+++ cmd/main.go
@@ -7,36 +7,48 @@ import (
 	"pokemon-battle/domain/model"
 )
 
-func main() {
-	// ポケモンの作成
+// createPokemon はポケモンを作成し、技を覚えさせます
+func createPokemon() (*model.Pokemon, *model.Pokemon, *model.Move, *model.Move, error) {
+	// ピカチュウの作成
 	pikachu, err := model.NewPokemon("ピカチュウ", 35, 55, 40, 90)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, nil, nil, fmt.Errorf("ピカチュウの作成に失敗: %w", err)
 	}
 
+	// フシギダネの作成
 	bulbasaur, err := model.NewPokemon("フシギダネ", 45, 49, 49, 45)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, nil, nil, fmt.Errorf("フシギダネの作成に失敗: %w", err)
 	}
 
 	// 技の作成
 	thunder, err := model.NewMove("10万ボルト", 90, 100)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, nil, nil, fmt.Errorf("10万ボルトの作成に失敗: %w", err)
 	}
 
 	tackle, err := model.NewMove("たいあたり", 40, 100)
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, nil, nil, fmt.Errorf("たいあたりの作成に失敗: %w", err)
 	}
 
 	// 技をポケモンに覚えさせる
 	if !pikachu.AddMove(thunder) {
-		log.Fatal("ピカチュウに技を覚えさせることができませんでした")
+		return nil, nil, nil, nil, fmt.Errorf("ピカチュウに技を覚えさせることができませんでした")
 	}
 
 	if !bulbasaur.AddMove(tackle) {
-		log.Fatal("フシギダネに技を覚えさせることができませんでした")
+		return nil, nil, nil, nil, fmt.Errorf("フシギダネに技を覚えさせることができませんでした")
+	}
+
+	return pikachu, bulbasaur, thunder, tackle, nil
+}
+
+func main() {
+	// ポケモンの作成と技の習得
+	pikachu, bulbasaur, thunder, tackle, err := createPokemon()
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	// バトルサービスの作成
